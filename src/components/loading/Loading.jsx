cbdee63e50bffcaed1641d3dac53f0cb47572e23
Ajay <ajayajuu73@gmail.com>
2024-01-24 21:02:18 +0530

    loading animation added

---
+++ src/components/loading/Loading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Loading() {
+  return (
+    <div className="justify-center items-center flex h-screen">
+      <div className="w-16 h-16 border-8  rounded-full animate-spin border-t-blue-600"></div>
+    </div>
+  );
+}
+
+export default Loading;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { BASE_URL } from "../Common";
 import axios from "axios";
+import Loading from "../components/loading/Loading";
 
 function Home() {
   const [todos, setTodos] = useState([]);
@@ -48,7 +49,7 @@ function Home() {
         },
       };
       const response = await axios(config);
-    
+
       if (response?.status === 201) {
         setTodos((prevTodos) => [...prevTodos, response?.data?.newTodo]);
         return;
@@ -62,7 +63,8 @@ function Home() {
     setAddTask(e.target.value);
   };
 
-  console.log(addTask, todos, "to");
+
+
 
   return (
     <div className="bg-[#d6cfcf] pt-5 text-[#000] min-h-[100vh]">
@@ -71,36 +73,43 @@ function Home() {
           {" "}
           TODO LIST
         </p>
-        <form className="mt-5" onSubmit={handleSubmit}>
-          <input
-            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none"
-            type="text"
-            value={addTask}
-            onChange={(e) => handleInputChange(e)}
-            placeholder="Add Task"
-          />
 
-          <div className="text-center">
-            <button className="px-5 py-2 rounded-xl font-[600] text-[#fff] bg-[blue]">
-              {" "}
-              Add Task{" "}
-            </button>
-          </div>
-        </form>
+        {todos?.length > 0 ? (
+          <>
+            <form className="mt-5" onSubmit={handleSubmit}>
+              <input
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none"
+                type="text"
+                value={addTask}
+                onChange={(e) => handleInputChange(e)}
+                placeholder="Add Task"
+              />
 
-        {todos?.map((item) => (
-          <div className="py-2 bg-gray-200 mt-10 text-gray-700 justify-between items-center flex">
-            <div className="text-[16px]  px-4 ">{item?.todo}</div>
-            <div className="items-center gap-5 flex pr-2 text-[#fff] text-[16px]">
-              <button className=" bg-[#4848cf] py-2  px-4 shadow-lg rounded-lg">
-                Edit
-              </button>
-              <button className=" bg-[#cb2828] py-2  px-4 shadow-lg rounded-lg">
-                Delete
-              </button>
-            </div>
-          </div>
-        ))}
+              <div className="text-center">
+                <button className="px-5 py-2 rounded-xl font-[600] text-[#fff] bg-[blue]">
+                  {" "}
+                  Add Task{" "}
+                </button>
+              </div>
+            </form>
+
+            {todos?.map((item) => (
+              <div className="py-2 bg-gray-200 mt-10 text-gray-700 justify-between items-center flex">
+                <div className="text-[16px]  px-4 ">{item?.todo}</div>
+                <div className="items-center gap-5 flex pr-2 text-[#fff] text-[16px]">
+                  <button className=" bg-[#4848cf] py-2  px-4 shadow-lg rounded-lg">
+                    Edit
+                  </button>
+                  <button className=" bg-[#cb2828] py-2  px-4 shadow-lg rounded-lg">
+                    Delete
+                  </button>
+                </div>
+              </div>
+            ))}
+          </>
+        ) : (
+          <Loading />
+        )}
       </div>
     </div>
   );
