c25037d9d1a3345fd090664001b5242d59136d96
Ajay <ajayajuu73@gmail.com>
2024-01-24 21:07:43 +0530

    loading animation added

--- src/components/loading/Loading.jsx
+++ src/components/loading/Loading.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Loading() {
   return (
-    <div className="justify-center items-center flex h-screen">
+    <div className="justify-center items-center flex min-h-[100vh]">
       <div className="w-16 h-16 border-8  rounded-full animate-spin border-t-blue-600"></div>
     </div>
   );
